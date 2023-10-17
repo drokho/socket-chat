e1748a29b1fe94670871e18276032b74440b51a8
Brandon Gamble <typhoongraphics@yahoo.com>
2023-10-16 22:09:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(new URL('C:/webdev/socket/index.html').pathname);
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+  });
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
